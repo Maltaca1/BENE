580219a19af312d0cbc96be05f35dace77befc36
ReidoS <142434043+Maltaca1@users.noreply.github.com>
2024-09-23 12:23:33 -0300

    f

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
-let nomeUsuario ='Bene,Cama';
-// alert (nomeUsuario);
-function soma (a,b) {
-    return a+b;
-    console.log(soma(1,2));
+let nomeUsuario ='kaio,ariel, gabi';
+
+// alert(nomeUsuario);
+function soma(a,b){
+return a+b;
 }
-function multiplicar (a,b) {
-    return a*b;
-    console.log(multiplicar(6,6));
+
+console.log("a soma de 6+6 é",soma(6,6));
+
+function multiplication(b,h){
+return b*h;
 }
-    
+
+console.log("um quarado d 9 por 9 a sua area é:",multiplication(9,9));
+
+
+
+let areaTriangulo = function(a,b){
+    return (a*b)/2;
+}
+console.log("se um lado do triangulo isoceles for 2 sua area é:",areaTriangulo(2,2));
+
+
+
+let num1 = parseInt(prompt('numero 1')) ;
+let num2= parseInt(prompt('numero 2')) ;
+let num3= parseInt(prompt('numero 3')) ;
+function media(a,b,c){
+    return ((a+b+c)/3);
+}
+document.write(media(num1, num2, num3));
