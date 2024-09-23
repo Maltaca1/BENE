4244dedbfdc60ff2169bd2cef5bdbb9a44773fd3
ReidoS <142434043+Maltaca1@users.noreply.github.com>
2024-09-23 11:45:22 -0300

    g

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ let nomeUsuario - 'BENECAMA';
 function soma (a,b) {
       return a+b;
   console.log(soma(1,2));
-     
+ {
 function multiplicar (a,b) {
     return a*b;
-    console.log(mulplicar(6,6));
+    console.log(multiplicar(6,6));
 }
