ebe5b3c95fbae57d2cd81c34518ec0df0a9f3927
ReidoS <142434043+Maltaca1@users.noreply.github.com>
2024-09-30 11:53:22 -0300

    c

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
 let nomeUsuario ='cristopher, neves';
 
-// alert(nomeUsuario);
-function soma(a,b){
-return a+b;
-}
+// // alert(nomeUsuario);
+// function soma(a,b){
+// return a+b;
+// }
 
-console.log("a soma de 6+6 é",soma(6,6));
+// console.log("a soma de 6+6 é",soma(6,6));
 
-function multiplication(b,h){
-return b*h;
-}
+// function multiplication(b,h){
+// return b*h;
+// }
 
-console.log("um quarado d 9 por 9 a sua area é:",multiplication(9,9));
-
-
-
-let areaTriangulo = function(a,b){
-    return (a*b)/2;
-}
-console.log("se um lado do triangulo isoceles for 2 sua area é:",areaTriangulo(2,2));
+// console.log("um quarado d 9 por 9 a sua area é:",multiplication(9,9));
 
 
 
-let num1 = parseInt(prompt('numero 1')) ;
-let num2= parseInt(prompt('numero 2')) ;
-let num3= parseInt(prompt('numero 3')) ;
-function media(a,b,c){
-    return ((a+b+c)/3);
-}
-document.write(media(num1, num2, num3));
+// let areaTriangulo = function(a,b){
+//     return (a*b)/2;
+// }
+// console.log("se um lado do triangulo isoceles for 2 sua area é:",areaTriangulo(2,2));
+
+
+
+// let num1 = parseInt(prompt('numero 1')) ;
+// let num2= parseInt(prompt('numero 2')) ;
+// let num3= parseInt(prompt('numero 3')) ;
+// function media(a,b,c){
+//     return ((a+b+c)/3);
+// }
+// document.write(media(num1, num2, num3));
+let estados = ['parana', 'sao paulo', 'minas gerais', 'paraiba', 'para', 'rio grande do sul', 'santa catarina', 'mato grosso do sul', 'mato grosso do norte', 'bahia', 'amazonia', 'acre', 'rondonia', 'goias',  'alagoas', 'ceara', 'maranhao', 'paraiba' , 'pernambuco', 'piaui', 'sergipe', 'amapa', 'roraima', 'tocantis', 'espirito santo']
